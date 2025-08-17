1f14a928fda9ad6c0ea922c4ef6a8d86b02f8c6f
elaine <195331169+elainehello@users.noreply.github.com>
2025-08-17 18:40:48 +0200

    dependency versions fixed

--- src/main/java/org/elainevalles/httpserver/HttpServer.java
+++ src/main/java/org/elainevalles/httpserver/HttpServer.java
@@ -1,9 +1,15 @@
 package org.elainevalles.httpserver;
+
+import org.elainevalles.httpserver.config.ConfigurationManager;
+
 /*
 * Driver Class for the HTTP Server
 * */
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("\nStarting HTTP server...");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+
     }
 }
