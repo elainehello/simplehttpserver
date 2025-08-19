d038deb2d1e722064b86614a205d60f686b0a9ef
elaine <195331169+elainehello@users.noreply.github.com>
2025-08-19 16:48:15 +0200

    port listening

--- src/main/java/org/elainevalles/httpserver/HttpServer.java
+++ src/main/java/org/elainevalles/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package org.elainevalles.httpserver;
 import org.elainevalles.httpserver.config.Configuration;
 import org.elainevalles.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /*
 * Driver Class for the HTTP Server
 * */
@@ -15,5 +21,24 @@ public class HttpServer {
         System.out.println("Current configuration: " + conf);
         System.out.println("Current port: " + conf.getPort());
         System.out.println("Current webroot: " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple HTTP Server</title></head>"
+                        + "<body><h1>Welcome to the Simple HTTP Server!</h1></body></html>";
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
